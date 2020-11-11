d3701a4b7fd8bec44fd2bfdff5f4863a01ed93a6
Mert Alnuaimi <snickns@gmail.com>
2020-11-11 11:24:22 +0300

    moved files to tf

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-azuretemplate-aks-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "${var.prefix}-azuretemplateVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["192.168.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-azuretemplatesubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["192.168.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.vn.name
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-azuretemplateaks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "azuretemplate-dns-prefix"
+
+  default_node_pool {
+    name           = "examplepool"
+    node_count     = 1
+    vm_size        = "Standard_D2_v2"
+    vnet_subnet_id = azurerm_subnet.subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "Standard"
+  }
+}
+
+data "azurerm_public_ip" "pip" {
+  name                = reverse(split("/", tolist(azurerm_kubernetes_cluster.aks.network_profile.0.load_balancer_profile.0.effective_outbound_ips)[0]))[0]
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
