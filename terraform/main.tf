ddd2f612831aef5035770ecaa08210c2a0adc2d6
Mert Alnuaimi <snickns@gmail.com>
2020-11-11 17:49:09 +0300

    Deploy 01 fixed typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "vn" {
   name                = "${var.prefix}-azuretemplateVnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = "192.168.0.0/16"
+  address_space       = ["192.168.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
