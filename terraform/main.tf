0426d2fb73028c3046491f3672be5ca4cc3076a8
Mert Alnuaimi <snickns@gmail.com>
2020-11-12 09:22:23 +0300

    Deploy 01 tstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,50 +31,50 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
 }
 
-# resource "azurerm_virtual_network" "vn" {
-#   name                = "${var.prefix}-Vnet"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   address_space       = ["192.168.0.0/16"]
-# }
+resource "azurerm_virtual_network" "vn" {
+  name                = "${var.prefix}-Vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["192.168.0.0/16"]
+}
 
-# resource "azurerm_subnet" "subnet" {
-#   name                 = "${var.prefix}-subnet"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   address_prefix       = "192.168.1.0/24"
-#   virtual_network_name = azurerm_virtual_network.vn.name
-#   service_endpoints    = ["Microsoft.Sql"]
-# }
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefix       = "192.168.1.0/24"
+  virtual_network_name = azurerm_virtual_network.vn.name
+  service_endpoints    = ["Microsoft.Sql"]
+}
 
-# resource "azurerm_kubernetes_cluster" "aks" {
-#   name                = "${var.prefix}-aks"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   dns_prefix          = "${var.prefix}-dns-prefix"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-dns-prefix"
 
-#   default_node_pool {
-#     name           = "${var.prefix}-pool"
-#     node_count     = 1
-#     vm_size        = "Standard_D2_v2"
-#     vnet_subnet_id = azurerm_subnet.subnet.id
-#   }
+  default_node_pool {
+    name           = "${var.prefix}-pool"
+    node_count     = 1
+    vm_size        = "Standard_D2_v2"
+    vnet_subnet_id = azurerm_subnet.subnet.id
+  }
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   network_profile {
-#     network_plugin    = "kubenet"
-#     load_balancer_sku = "Standard"
-#   }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "Standard"
+  }
 
-#   service_principal {
-#     client_id     = var.client_id
-#     client_secret = var.client_secret
-#   }
-# }
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+}
 
-# data "azurerm_public_ip" "pip" {
-#   name                = reverse(split("/", tolist(azurerm_kubernetes_cluster.aks.network_profile.0.load_balancer_profile.0.effective_outbound_ips)[0]))[0]
-#   resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
-# }
+data "azurerm_public_ip" "pip" {
+  name                = reverse(split("/", tolist(azurerm_kubernetes_cluster.aks.network_profile.0.load_balancer_profile.0.effective_outbound_ips)[0]))[0]
+  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+}
