0ae0d27e24b567a83f7dc0339980cc1e632a978f
Mert Alnuaimi <snickns@gmail.com>
2020-11-12 09:50:56 +0300

    Deploy 01 changed pool name

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = "${var.prefix}-dns-prefix"
 
   default_node_pool {
-    name           = "$aztepool"
+    name           = "aztepool"
     node_count     = 1
     vm_size        = "Standard_D2_v2"
     vnet_subnet_id = azurerm_subnet.subnet.id
