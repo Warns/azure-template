3d37a0ebbced6bb3f581e69905a88216242ee7d3
Mert Alnuaimi <snickns@gmail.com>
2020-11-12 09:48:48 +0300

    Deploy 01 changed pool name

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
+  admin_enabled       = "true"
 }
 
 resource "azurerm_virtual_network" "vn" {
